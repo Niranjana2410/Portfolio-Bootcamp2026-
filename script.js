3825a9e35ac5b38d8f4ab97c37f3b18d803177c0
Niranjana2410 <niranju909@gmail.com>
2026-06-25 01:22:46 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let name = prompt("What is your name?")
 alert("Hello " + name + "! Welcome to my portfolio page.")
+const form = document.querySelector('form');
+    const hamburger = document.getElementById('hamburger');
+const navUl = document.querySelector('nav ul');
+      hamburger.addEventListener('click', () => {
+         navUl.classList.toggle('show');
+}
+    );
